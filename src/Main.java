f4f477261506f94b79eb98717174fe10265be71f
jamez7 <kulikowskijacob@gmail.com>
2024-04-06 18:56:58 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
-    public static void main(String[] args)
-       int x = 1;
+    public static void main(String[] args){
+
+        Point point = new Point();
+        point.setX(6.5);
+        point.setY(2.5);
+        System.out.println("Wartość pola x = " + point.getX());
+        System.out.println("Wartość pola y = " + point.getY());
+
+
+
+    }
+
 }
--- src/Point.java
+++ src/Point.java
@@ -2,6 +2,20 @@ public class Point {
     private double x;
     private double y;
 
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+
 
 
 }
