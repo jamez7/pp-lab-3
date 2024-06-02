ada9df1c733c73dfeb1dd989659a4420cb950613
jamez7 <kulikowskijacob@gmail.com>
2024-06-02 16:37:45 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,17 @@
 import geometry.Circle;
 import geometry.ColoredCircle;
 import geometry.Point;
+import geometry.Rectangle;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
 
-        ArrayList<Object> circles = new ArrayList<Object>();
+        Rectangle rectangle = new Rectangle(10,7);
+        System.out.println("Area of rectangle: " + rectangle.calculateArea());
+        System.out.println("Perimeter of rectangle: " + rectangle.calculatePerimeter());
 
-        Circle circle = new Circle(new Point(9, 8), 11);
-        Circle circle2 = new Circle(new Point(2, 1), 3);
-        ColoredCircle circle3 = new ColoredCircle(new Point(8, 9), 10, "green");
-        ColoredCircle circle4 = new ColoredCircle(new Point(3, 20), 14, "red");
-
-        circles.add(circle);
-        circles.add(circle2);
-        circles.add(circle3);
-        circles.add(circle4);
-
-        for (Object c : circles) {
-            if (c instanceof ColoredCircle) {
-
-                ColoredCircle cc = (ColoredCircle) c;
-                System.out.println("Circle is colored, color: " + cc.getColor());
-                System.out.println(cc.getRadius());
-                System.out.println(cc.getCenter());
-
-            } else {
-                Circle cc = (Circle) c;
-                System.out.println("Circle is not colored");
-                System.out.println(cc.getRadius());
-                System.out.println(cc.getCenter());
-            }
-
-        }
 
         
 
---
+++ src/geometry/Rectangle.java
@@ -0,0 +1,35 @@
+package geometry;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double getLength() {
+        return this.length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return this.length * this.width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * this.length + 2 * this.width;
+    }
+}
+
