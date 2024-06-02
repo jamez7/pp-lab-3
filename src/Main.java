c6a7f6295e500bc60cc2426f5336dfe7ebcbb9ad
jamez7 <kulikowskijacob@gmail.com>
2024-06-02 10:38:32 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import geometry.Circle;
+import geometry.ColoredCircle;
 import geometry.Point;
 
 public class Main {
@@ -6,7 +7,11 @@ public class Main {
 
 
         Point point = new Point(5,8);
-        Circle circle = new Circle(point, 11);
+        ColoredCircle circle = new ColoredCircle(point, 11, "green");
+        System.out.println("Circle color: " + circle.getColor());
+        System.out.println("Circle radius: " + circle.getRadius());
+        System.out.println("Circle center: x= " + circle.getCenter().getX() + ", y= " + circle.getCenter().getY());
+
 
         System.out.println(circle.calculatePerimeter());
 
