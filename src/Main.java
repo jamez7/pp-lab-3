8c8e9761e4ea7024d2aa3fd4f7f5c5c737c69acd
jamez7 <kulikowskijacob@gmail.com>
2024-06-02 16:40:48 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
-import geometry.Circle;
-import geometry.ColoredCircle;
-import geometry.Point;
-import geometry.Rectangle;
+import geometry.*;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
 
-        Rectangle rectangle = new Rectangle(10,7);
-        System.out.println("Area of rectangle: " + rectangle.calculateArea());
-        System.out.println("Perimeter of rectangle: " + rectangle.calculatePerimeter());
+        Square square = new Square(5);
+        System.out.println("Area of the square: " + square.calculateArea());
+        System.out.println("Perimeter of the square: " + square.calculatePerimeter());
 
-
-        
 
     }
 
